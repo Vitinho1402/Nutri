eff90b7ff5a0d1b3830cd62e6f3b0f26c7bb5212
Vitinho1402 <kaua.pinto.costa@escola.pr.gov.br>
2024-11-06 21:51:23 -0300

    Add files via upload

---
+++ sistemaNutri/paciente.js
@@ -0,0 +1,12 @@
+class paciente {
+    constructor(nome,idade,ano,altura,genero,percentual) {
+        this.nome = nome
+        this.idade = idade
+        this.ano = ano
+        this.altura = altura
+        this.genero = genero
+        this.percentual = percentual
+    }
+    
+}
+export {paciente}
